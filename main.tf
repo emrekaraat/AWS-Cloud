e8c1a27db174fe6f65192f4f7b78575925d35b95
Emre Karaat <77414802+emrekaraat@users.noreply.github.com>
2025-03-07 17:28:12 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,25 @@
 # Create a VPC to launch our instances into
-resource "aws_vpc" "dev_vpc" {
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"  
   enable_dns_hostnames = true 
   enable_dns_support = true
   
   tags       =  {
-    name     = "deham9"
+    name     = "emre-vpc"
+    dept     = "AWS"
+    costcenter = "CS100"
   }       
+}
+
+# create a subnetinside the VPC
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  
+  tags       =  {
+    name     = "emre-subnet"
+    dept     = "AWS"
+    costcenter = "CS100"
+  }
+  depends_on = [aws_vpc.main]
 }
